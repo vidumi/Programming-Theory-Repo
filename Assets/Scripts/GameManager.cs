6a4702779b6f218fb3387c8afd19e28ec728f8f3
Vincenzo Duminuco <v.duminuco@gmail.com>
2021-10-25 19:32:08 +0200

    Objects To Find

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public GameObject m_GameOverPanel;
+    public Text m_ScoreText;
+    public Text m_LivesText;
+
+    private bool paused;
+
+    public int lives = 3;
+    private int score;
 
     private void Awake()
     {
@@ -21,7 +29,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // UpadateLives(3);
+        m_LivesText.text = "Lives: " + lives;
     }
 
     // Update is called once per frame
@@ -30,8 +39,37 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        m_ScoreText.text = ("Score: " + score);
+    }
+    public void UpadateLives(int livesTochange)
+    {
+        lives += livesTochange;
+        m_LivesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+
+    }
     public void GameOver()
     {
         m_GameOverPanel.SetActive(true);
+        ChangePaused();
+    }
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            Time.timeScale = 1;
+        }
     }
 }
--- Assets/Scripts/ObjectsToFind.cs
+++ Assets/Scripts/ObjectsToFind.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class ObjectsToFind : MonoBehaviour
 {
-    public float speed = 1.0f;
-    private Rigidbody enemyRb;
-    private GameObject player;
+
     private GameManager gameManager;
+    public int objectValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody>();
-        player = GameObject.Find("Player");
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
     }
@@ -21,16 +18,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed);
 
     }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            gameManager.UpadateLives(-1);
+            gameManager.UpdateScore(objectValue);
+            Destroy(gameObject);
+            Debug.Log("value; " + objectValue);
         }
     }
 
